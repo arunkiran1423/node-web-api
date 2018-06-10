927345a2255d28c7409a1f012e3d0be14432a1a1
arun <arunkiran1423@gmail.com>
2018-06-10 03:51:46 -0700

    Initial commit

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,19 +7,29 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').find().toArray().then((doc) =>{
-    console.log('Todos')
-    console.log (JSON.stringify(doc,undefined,2))
-  },(err) =>{
-    console.log('unable to fetch todos',err)
+//   db.collection('Todos').find().toArray().then((doc) =>{
+//     console.log('Todos')
+//     console.log (JSON.stringify(doc,undefined,2))
+//   },(err) =>{
+//     console.log('unable to fetch todos',err)
+//
+// })
+//
+// db.collection('Users').find({name : 'Andrew'}).count().then((user) =>{
+//   console.log(`count : ${user}`)
+//   console.log(JSON.stringify(user,undefined,2))
+// },(err) =>{
+//   console.log('it is error')
+// })
 
+db.collection('Todos').deleteMany({completed: true}).then((result) =>{
+  console.log((Json.stringify(result,undefined,2)
 })
 
-db.collection('Users').find({name : 'Andrew'}).count().then((user) =>{
-  console.log(`count : ${user}`)
-  console.log(JSON.stringify(user,undefined,2))
-},(err) =>{
-  console.log('it is error')
+db.collection('Users').findOneAndDelete({_id:new ObjectID("5b1cf5ff06532d5540a00439")}).then((result)=>{
+  console.log(JSON.stringify(result,undefined,2))
+})
+
 })
 
   //db.close();
